1d4bcbea94ccaf3632bef7ef6ba2411ffa4174a1
MIGHTY1o1 <shubhagarwalcse@gmail.com>
2024-10-08 21:48:06 +0530

    add changes tomake code workable in locale

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 dotenv.config();
 connectDB();
+
+const SERVER_PORT = process.env.SERVER_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
 });
